1566fda66f8eba9240a71cb9fb8df90e3a323940
zjiang4 <33261242+zjiang4@users.noreply.github.com>
2019-10-30 19:16:41 -0500

    103019output

--- R/StanPosterior.attribute.R
+++ R/StanPosterior.attribute.R
@@ -1,9 +1,22 @@
 StanPosterior.attribute<-function(stan.model,all.only=T){
   nc=length(StanPosterior.class(stan.model))
+  class.name<-unique(apply(combn(rep(c(0,1),log2(nc)),log2(nc)),2,function(x){paste(x,collapse = "")}))
   posteriorAttr<-get_posterior_mean(stan.model,pars = c("posteriorPC"))
+  rowNames<-row.names(posteriorAttr)
+  newRowNames<-rowNames
+  for(i in 1: length(newRowNames)){
+    id<-str_match_all(rowNames[i],"[0-9]+")[[1]][,1]
+    newRowNames[i]<-paste(paste("p",id[1],sep=''),paste("c",class.name[as.numeric(as.character(id[2]))],sep=''),sep='')
+  } 
   np=nrow(posteriorAttr)/nc
   if(all.only){
     posteriorAttr<-unlist(lapply(1:np,function(x){which.max(matrix(posteriorAttr[,ncol(posteriorAttr)],np,nc,byrow = T)[x,])}))
+    
+    posteriorAttr<-class.name[posteriorAttr]
+  }
+  if(!all.only){
+    row.names(posteriorAttr)<-newRowNames
+    
   }
   posteriorAttr
 }
--- R/StanPosterior.class.R
+++ R/StanPosterior.class.R
@@ -1,7 +1,11 @@
 StanPosterior.class<-function(stan.model,all.only=T){
   res<-get_posterior_mean(stan.model,pars = c("Vc"))
+  nc<-nrow(res)
+  class.name<-unique(apply(combn(rep(c(0,1),log2(nc)),log2(nc)),2,function(x){paste(x,collapse = "")}))
+  rownames(res)<-class.name
   if(all.only){
     res<-res[,ncol(res)]
+    
   }
   res
 }
