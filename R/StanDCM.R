f71ae387b93439e17622f5da58671cf7aff4ce31
zjiang4 <jiangzhehan@gmail.com>
2020-06-23 20:38:00 +0800

    Update StanDCM.R

--- R/StanDCM.R
+++ R/StanDCM.R
@@ -98,7 +98,7 @@ StanPosterior.item<-function(stan.model,lambda.parm=TRUE){
   summaryStan<-summary(x)$'summary'
   temp<-!grepl("PImat|log_lik|contributionsI|posteriorIC|Vc|posteriorPC|lp",rownames(summaryStan))  
   summaryStan<-summaryStan[temp,]
-  if(lambda.parm=TRUE){
+  if(lambda.parm==TRUE){
     summaryStan[grepl("l",rownames(summaryStan)),]
   }else{
     summaryStan[!grepl("l",rownames(summaryStan)),]
